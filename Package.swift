35cf8e3acf874c0fb16556b30853e15fe05206b6
Arun_Pandian <arun.pandian@myn.team>
2024-05-22 17:59:09 +0530

    Package issues resolved

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     targets: [
         .binaryTarget(
                  name: "ActualSDK",
-                 url: "https://drive.google.com/uc?export=download&id=1Gi9VyRt11eY3S4gnqbDNiAfT1bEIBUoO",
+                 url: "https://github.com/ArunPIos/ActualSDK/releases/download/1.0.0/ActualSDK.xcframework.zip",
                  checksum: "220455bfba25f9882fc477cf5903302acdf5c325a3a45914b11e3ee70666b38c"
              )
     ]
